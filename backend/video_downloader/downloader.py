396bc5ef8736c3ff3820ae810df45de4988f6c37
Mohit-majumdar <majumdarmohit12345@gmail.com>
2024-07-23 01:23:34 +0530

    fixed: 400 Bad request

--- backend/video_downloader/downloader.py
+++ backend/video_downloader/downloader.py
@@ -2,6 +2,14 @@ import os
 from typing import Dict, List
 from pytube import YouTube
 from pathlib import Path
+from pytube.innertube import _default_clients
+
+_default_clients["ANDROID"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["ANDROID_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_EMBED"]["context"]["client"]["clientVersion"] = "19.08.35"
+_default_clients["IOS_MUSIC"]["context"]["client"]["clientVersion"] = "6.41"
+_default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID_CREATOR"]
 
 VIDEO_PATH = Path(".").absolute() / "videos"
 VIDEO_PATH.mkdir(exist_ok=True)
